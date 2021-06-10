a9a44ec262adc44c36515bac05d82120d402bfaa
Антон Смирнов <icescream1992@gmail.com>
2021-06-10 13:19:14 +0300

    изменил иконку no image

--- GuestBook/GuestBook/Networking/Websockets/Configuration.swift
+++ GuestBook/GuestBook/Networking/Websockets/Configuration.swift
@@ -7,3 +7,10 @@
 
 import Foundation
 
+
+struct Configuration {
+    static let scheme = "https"
+    static let host = "guest-book.naveksoft.com"
+    static let baseURL = "https://guest-book.naveksoft.com:6001"
+    static let path = ":6001"
+}
